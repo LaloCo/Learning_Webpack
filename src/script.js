2e5e21da596b8fdbc032633cff53245d3c3cb9bf
Eduardo Rosas <lalorosas@outlook.com>
2021-04-18 11:52:30 -0500

    Explanation about script.hs

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
 import './style.css'
+
+// this is the root of the project, not index.html
+// that means that we don't need to add this script
+// to the index.html for it to work, and since this
+// file is importing thee styles.css, we don't need
+// to add it to the html file either.
